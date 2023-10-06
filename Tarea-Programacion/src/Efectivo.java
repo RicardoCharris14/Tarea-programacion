0ff2b6f150921943202948758ebc2abb0eb7aa1b
VicenteRamirez15 <vramirezt15@gmail.com>
2023-10-05 22:35:43 -0300

    Compleción OrdenCompra avance 2

--- Tarea-Programacion/src/Efectivo.java
+++ Tarea-Programacion/src/Efectivo.java
@@ -19,5 +19,4 @@ public class Efectivo extends Pago {
         }
     }
 
-
 }
--- Tarea-Programacion/src/OrdenCompra.java
+++ Tarea-Programacion/src/OrdenCompra.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 public class OrdenCompra {
     private Date fecha;
     private String estado;
+    private float  porPagar;
     private ArrayList<DetalleOrden> listaCompras;
+    private  ArrayList<Pago> pagos;
+
     public OrdenCompra(Date fecha, String estado) {
         this.fecha = fecha;
         this.estado = estado;
+        porPagar = 0;
+
     }
 
     //-------getters and setters-----------
@@ -26,9 +31,17 @@ public class OrdenCompra {
     public void setEstado(String estado) {
         this.estado = estado;
     }
-    public void setListaCompras(DetalleOrden orden){
+    public void añadirOrden(DetalleOrden orden){
         listaCompras.add(orden);
+        porPagar += orden.calcPrecio();
+
     }
+
+    public void añadirPago(Pago pago){
+        pagos.add(pago);
+    }
+
+
     public float calcPrecioSinIVA( ){
         float precio = 0;
         for (DetalleOrden listaCompra : listaCompras) {
@@ -44,12 +57,38 @@ public class OrdenCompra {
         return precio;
     }
     public float calcPrecio(){
-        
+        float precio = 0;
+        for (DetalleOrden listaCompra : listaCompras){
+            precio += listaCompra.calcPrecio();
+        }
+
+        return precio;
+
     }
     public float calcPeso(){
-
+        float peso = 0;
+        for (DetalleOrden listaCompra : listaCompras){
+            peso += listaCompra.calcPeso();
+        }
+        return peso;
     }
 
+
+    public String pagar(){
+        float pagoTotal=0;
+        for (int i=0; i<pagos.size(); i++){
+            pagoTotal += pagos.remove(0).getMonto();
+        }
+        porPagar -= pagoTotal;
+        if (porPagar==0){
+            return "Pago completado exitosamente: el total de su compra ha sido pagada.";
+        }
+        else{
+            return "Pago completado exitosmente: su deuda actual es de " + porPagar;
+
+        }
+
+    }
 
 
 }
